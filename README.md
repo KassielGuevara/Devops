09c9cfbccd38334b38c83063c816d8d0f12fd8b6
Rodrigo Ribas <kassielferreira@gmail.com>
2026-04-14 17:32:45 -0300

    Inicializa projeto conversor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Devops
 Trabalho Devops
+
+
+# Conversor de Moedas
+
+Projeto simples em Python para prática de CI/CD e Docker.
---
+++ app.py
@@ -0,0 +1,33 @@
+from conversor import *
+import os
+
+def menu():
+    while True:
+        print("\n--- CONVERSOR DE MOEDAS ---")
+        print("1 - Real para Dólar")
+        print("2 - Real para Euro")
+        print("3 - Ver histórico")
+        print("0 - Sair")
+
+        opcao = input("Escolha: ")
+
+        if opcao == "1":
+            valor = float(input("Valor em reais: "))
+            print(real_para_dolar(valor))
+
+        elif opcao == "2":
+            valor = float(input("Valor em reais: "))
+            print(real_para_euro(valor))
+
+        elif opcao == "3":
+            for h in mostrar_historico():
+                print(h)
+
+        elif opcao == "0":
+            break
+
+if __name__ == "__main__":
+    if os.getenv("CI"):
+        print("Executando no CI com sucesso!")
+    else:
+        menu()
